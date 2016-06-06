125d9569044a489b6faa477ce7a8b2802e1bbf34
mohit-nyllabs <mohit@nyllabs.tech>
2016-06-06 17:01:13 -0400

    component+data binding III

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -22,7 +22,22 @@ import { Component } from '@angular/core';
             </section>
         </div>
        
-    `
+    `,
+    styles:[`
+        .author {
+        
+        display: inline-block;
+        font-style: italic;
+        font-size: 12px;
+        width: 80%;
+        }
+        .config{
+        display: inline-block;
+        text-align: right;
+        font-size: 12px;
+        width: 19%;
+        }
+`]
 })
 export class AppComponent {
     message =
