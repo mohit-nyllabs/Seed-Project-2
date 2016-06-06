392b2f1c1a8eda19757d58cc16d290a04edca72f
mohit-nyllabs <mohit@nyllabs.tech>
2016-06-06 17:23:06 -0400

    one-way binding added

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: ` 
+         <div class="row">
+             <section class ="col-md-8 col-md-offset-2">
+                <input type="text" [(ngModel)] ="message.content">
+            </section>
+        </div>
         <div class = "row">
             <section class ="col-md-8 col-md-offset-2">
                 <article class ="panel panel-default">
