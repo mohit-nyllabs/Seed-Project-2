93c249973fa67d441197affabd11ede1da45a4e3
mohit-nyllabs <mohit@nyllabs.tech>
2016-06-07 15:58:57 -0400

    using ngFor

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -10,7 +10,7 @@ import {Message} from "./messages/message";
         </div>
         <div class = "row">
             <section class ="col-md-8 col-md-offset-2">
-               <my-message [message]="message" (editClicked)="message.content = $event"></my-message>
+               <my-message *ngFor="let message of messages" [message]="message" (editClicked)="message.content = $event"></my-message>
             </section>
         </div>
        
@@ -18,7 +18,9 @@ import {Message} from "./messages/message";
     directives: [MessageComponent]
 })
 export class AppComponent {
-    message: Message = new Message('A new message', null, 'Max')
-
+   // message: Message = new Message('A new message', null, 'Max')
+    messages: Message[] = [
+        new Message('A new message', null, 'Max'),
+        new Message('Another message', null, 'Anna')];
 
 }
