19ce71240bd5a879efed026c06db23a5960541d1
mohit-nyllabs <mohit@nyllabs.tech>
2016-06-19 17:16:08 -0400

    backend getting messages

--- routes/messages.js
+++ routes/messages.js
@@ -3,6 +3,26 @@ var router = express.Router();
 
 var Message = require('../models/message')
 
+router.get('/', function(req, res, next){
+    // dont execute this and execute previous to this
+    Message.find()
+        .exec(function(err, docs){
+            if(err){
+                return res.status(404).json({
+                    title: ' An Error occured',
+                    error: err
+                });
+
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: docs
+            });
+        });
+});
+
+
+
 router.post('/', function(req, res , next){
     var message = new Message({
         content: req.body.content
@@ -21,6 +41,8 @@ router.post('/', function(req, res , next){
     });
 
 });
+
+
 
 
 
